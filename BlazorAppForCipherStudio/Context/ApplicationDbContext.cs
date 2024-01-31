27ae0d8593047670df65b81e640d6ae97b210e89
ShreeSoftwareDeveloper <shreenandansingh1234@gmail.com>
2024-02-01 00:30:08 +0530

    First

---
+++ BlazorAppForCipherStudio/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BlazorAppForCipherStudio.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppForCipherStudio.Context
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ BlazorAppForCipherStudio/Data/Service/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using BlazorAppForCipherStudio.Context;
+using BlazorAppForCipherStudio.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorAppForCipherStudio.Data.Service
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+        public EmployeeService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        //Get
+        public async Task<List<Employee>> GetEmployees()
+        {
+            return await applicationDbContext.Employees.ToListAsync();
+
+        }
+        //Add Employee
+        public async Task <bool>AddEmployee(Employee employee)
+        {
+            await applicationDbContext.Employees.AddAsync(employee); ;
+            await applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        //Get By Id
+        public async Task<Employee>GetById(int Id)
+
+        {
+            Employee employee = await applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == Id);
+            return employee;
+ 
+
+        }
+        //Update
+        public async  Task<bool>UpdateEmployeeDeatils(Employee employee)
+        {
+            applicationDbContext.Employees.Update(employee);
+            await applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+        //Delete
+        public async Task<bool> DeleteEmployee(Employee emmployee)
+        {
+            applicationDbContext.Employees.Remove(emmployee);
+            await applicationDbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
